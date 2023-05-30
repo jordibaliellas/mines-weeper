5cd9894233d8f85f22f40dd959ca000ffa8d257b
jordi baliellas <jordi.bp@endeavour.nl>
2023-05-30 18:04:45 +0100

    Create page profile

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,10 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+export default function Profile() {
+    return (
+        <Box>
+            <Typography>Profile</Typography>
+        </Box>
+    )
+}
