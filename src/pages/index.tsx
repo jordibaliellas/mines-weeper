bfe351d6506584f514190947543b7a3333f32d36
jordi baliellas <jordi.bp@endeavour.nl>
2023-05-31 12:21:23 +0100

    Install redux

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,14 @@
-import { MinesWeeperBoard, MinesWeeperCell } from '@/interfaces/minesweeper'
-import {
-    generateBoard,
-    generateVoidBoard,
-} from '@/factories/minesweeper.factory'
-import { useEffect, useState } from 'react'
-
 import Box from '@mui/material/Box'
 import MinesWeeper from '@/components/MinesWeeper'
+import { Provider } from 'react-redux'
+import { store } from '@/state/store'
 
 export default function Home() {
-    const [firstClick, setFirstClick] = useState(true)
-
-    const [minesWeeperConfig, setMinesWeeperConfig] = useState({
-        columns: 8,
-        rows: 8,
-        mines: 10,
-    })
-    const [board, setBoard] = useState<MinesWeeperBoard>([])
-
-    useEffect(() => {
-        setBoard(generateVoidBoard(minesWeeperConfig))
-    }, [minesWeeperConfig])
-
-    const updateBoard = (cellClicked: MinesWeeperCell) => {
-        if (firstClick) {
-            setFirstClick(false)
-            return setBoard(generateBoard(cellClicked, minesWeeperConfig))
-        }
-        if (cellClicked.isRevealed) return
-
-        setBoard((currentBoard) => {
-            const newBoard = [...currentBoard]
-            newBoard[cellClicked.row] = newBoard[cellClicked.row].map(
-                (cell) => {
-                    if (cell.column !== cellClicked.column) return cell
-                    return { ...cell, isRevealed: true }
-                }
-            )
-            return newBoard
-        })
-    }
-
     return (
-        <Box>
-            <MinesWeeper board={board} onCellClick={updateBoard}></MinesWeeper>
-        </Box>
+        <Provider store={store}>
+            <Box>
+                <MinesWeeper></MinesWeeper>
+            </Box>
+        </Provider>
     )
 }
